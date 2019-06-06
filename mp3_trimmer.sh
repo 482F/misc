9f74669c122afbc922b2638244313cf760588ee5
482F <ryo.ishikawa.2j@stu.hosei.ac.jp>
2019-06-06 14:38:52 +0900

    ローカル変数を使うように

--- mp3_trimmer.sh
+++ mp3_trimmer.sh
@@ -3,16 +3,16 @@
 set -ue -o pipefail
 
 function main(){
-    FILEPATH="${1}"
-    DURATION=$(calc_duration ${FILEPATH})
+    local FILEPATH="${1}"
+    local DURATION=$(calc_duration ${FILEPATH})
 
     if [ "${DURATION}" == "" ]; then
         echo "target file is not valid mp3 file"
         exit 1
     fi
 
-    stime="0"
-    index=1
+    local stime="0"
+    local index=1
 
     for etime in ${@:2}; do
         trim_mp3 "${FILEPATH}" "$(name_outfile ${FILEPATH} ${index})" "${stime}" "${etime}"
@@ -22,23 +22,28 @@ function main(){
     trim_mp3 "${FILEPATH}" "$(name_outfile ${FILEPATH} ${index})" "${stime}" "${DURATION}"
 }
 
+function time_to_sec(){
+    local TIME="${1}"
+}
+
 function calc_duration(){
     ffmpeg -i ${FILEPATH} 2>&1 | grep "Duration" | cut -d ' ' -f 4 | sed s/,// | awk '{ split($1, A, ":"); split(A[3], B, "."); print 3600*A[1] + 60*A[2] + A[3]}' || true
 }
+
 function name_outfile(){
-    FILEPATH="${1}"
-    INDEX="${2}"
-    FILENAME="${FILEPATH%.*}"
-    FILEEXT="${FILEPATH##*.}"
+    local FILEPATH="${1}"
+    local INDEX="${2}"
+    local FILENAME="${FILEPATH%.*}"
+    local FILEEXT="${FILEPATH##*.}"
     echo "${FILENAME}_${INDEX}.${FILEEXT}"
 }
 
 function trim_mp3(){
-    TARGETFILEPATH="${1}"
-    OUTPUTFILEPATH="${2}"
-    START_TIME="${3}"
-    END_TIME="${4}"
-    TIME_DIFF=$(echo "scale=2; ${END_TIME} - ${START_TIME}" | bc)
+    local TARGETFILEPATH="${1}"
+    local OUTPUTFILEPATH="${2}"
+    local START_TIME="${3}"
+    local END_TIME="${4}"
+    local TIME_DIFF=$(echo "scale=2; ${END_TIME} - ${START_TIME}" | bc)
     ffmpeg -i "${TARGETFILEPATH}" -ss "${START_TIME}" -t "${TIME_DIFF}" "${OUTPUTFILEPATH}"
 }
 

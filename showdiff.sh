95201575b27ccb513f67abf7576b0000e273a1c3
482F <ryo.ishikawa.2j@stu.hosei.ac.jp>
2019-06-08 11:02:33 +0900

    1 から古い > 新しい順に表示するように。また、存在しないコミットが指定されたときはエラーをはくように

--- showdiff.sh
+++ showdiff.sh
@@ -14,10 +14,16 @@ function main(){
         filepath="./${filepath}"
     fi
 
-    local num_a="${2}"
+    local num="${2}"
+    local NoC="$(number_of_commits)"
+    local num_a="$((${NoC}-${num}))"
     local num_b=$((num_a+1))
-    local content_a=$(n-th_content "${filepath}" "${num_a}")
-    local content_b=$(n-th_content "${filepath}" "${num_b}")
+    if [ ${num_a} -lt 0 ] || [ ${NoC} -lt ${num_b} ]; then
+        echo "there is no commit number \"${num}\". please set valid number: 1-$((${NoC}-1))" 1>&2
+        exit 1
+    fi
+    local content_a=$(n-th_content "${filepath}" "${num_a}" "${num}")
+    local content_b=$(n-th_content "${filepath}" "${num_b}" "${num}")
     echo "${content_a}" > /tmp/showdiff_content_a
     echo "${content_b}" > /tmp/showdiff_content_b
     vim -c "r! git log --skip ${num_a} -n 1" -c "w! /tmp/showdiff_commitlog | tabe | e /tmp/showdiff_content_a | vnew | e /tmp/showdiff_content_b | diffthis | normal " -c "diffthis" -c "set wrap | normal " -c "set wrap"
@@ -26,12 +32,23 @@ function main(){
 function n-th_content(){
     local filepath="${1}"
     local n="${2}"
-    git show $(n-th_hash "${n}"):"${filepath}"
+    local on="${3}"
+    local hash=$(n-th_hash "${n}" "${on}")
+    if [ "${hash}" == "" ]; then
+        exit 1
+    fi
+    git show "${hash}":"${filepath}" 2>/dev/null
 }
 
 function n-th_hash(){
     local n="${1}"
-    git log -1 --format='%h' --skip "${n}"
+    local on="${2}"
+    local hash=$(git log -1 --format='%h' --skip "${n}")
+    echo "${hash}"
+}
+
+function number_of_commits(){
+    git log --format="%h" | wc -l
 }
 
 main "${@}"

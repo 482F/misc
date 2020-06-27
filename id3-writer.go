827d61a0f8e477c13330e7f41917e2d5cc0f835c
482F <482F.git.mail@482f.net>
2020-06-28 00:00:00 +0900

    進捗の曲名がうまく描画されていなかったのを修正

--- id3-writer.go
+++ id3-writer.go
@@ -209,7 +209,7 @@ func main(){
     readFiles(id3DataArr, tmpFileBodies)
     for ind, id := range id3DataArr{
         id.writeMP3File()
-        fmt.Printf("\r%d/%d: %s", ind+1, NoID, id.Path)
+        fmt.Printf("\033[1K\r%d/%d: %s", ind+1, NoID, id.Path)
     }
     fmt.Println("")
     for k := 0; k < 4; k++{

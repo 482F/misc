0926f6c7f987413b78448fb2851b9a22de7679b5
482F <ryo.ishikawa.2j@stu.hosei.ac.jp>
2020-06-29 12:42:49 +0900

    いろいろ変更
    進捗を表示するように
    変更がないファイルに関しては出力しないように
    ディレクトリ指定で中の mp3 ファイルを再帰的に探索するように

--- set-mp3-gain-value.sh
+++ set-mp3-gain-value.sh
@@ -5,9 +5,35 @@ export LC_ALL=C
 
 VALUE="${1}"
 DIFF=$((${VALUE}-89))
+ind=1
+echo
 
 shift 1
+function expand-files(){
+    for file in "${@}"; do
+        if [ -d "${file}" ]; then
+            find "${file}" | grep -E *\.mp3
+        elif [ -f "${file}" ]; then
+            echo "${file}"
+        else
+            echo "\"${file}\" is not found"
+            exit 1
+        fi
+    done
+}
+function aacgain-func(){
+    echo -e "\e[1F${ind}/${NoF}"
+    ind=$((ind+1))
+    res="$(aacgain -r -c -p -d "${1}" "${2}")"
+    if ! echo "${res}" | grep -Eq "No changes to"; then
+        echo "${res}"
+    fi
+}
 
-for file in "${@}"; do
-    aacgain -r -c -p -d "${DIFF}" "${file}"
+EXPANDED_FILES="$(expand-files "${@}")"
+NoF="$(echo "${EXPANDED_FILES}" | wc -l)"
+
+echo "${EXPANDED_FILES}" | while read file; do
+    aacgain-func "${DIFF}" "${file}"
 done
+echo

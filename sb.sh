4700a8dc9873868b9871403d20b103d37e4ae97a
482F <482F.git.mail@482f.net>
2024-01-21 00:00:00 +0900

    sb を使って実行するスクリプトを /usr/local/bin/ 下に作成する機能を追加

--- sb.sh
+++ sb.sh
@@ -2,13 +2,32 @@
 set -ue -o pipefail
 
 function main() {
-  local filepath="${1:-}"
+  local opt=''
+  if [[ "${1:-}" == "-"* ]]; then
+    opt="${1}"
+    shift 1
+  else
+    opt='--exec'
+  fi
 
-  if [[ "${filepath}" == "" ]]; then
+  local filepath="${1:-}"
+  shift 1
+
+  if [[ -z "${filepath}" ]]; then
+    echo filepath is empty
     exit 1
   fi
 
-  local shebang="$()"
+  case "${opt}" in
+    -x | --exec) sb_exec "${filepath}" "${@}";;
+    -l | --link) sb_link "${filepath}" "${@}";;
+  esac
+
+  exit 0
+}
+
+function sb_exec() {
+  local filepath="${1:-}"
 
   local shebang_exe="$(head -n 1 "${filepath}" | grep -Po "(?<=^#\!).+" || true)"
   if [[ "${shebang_exe}" == "" ]]; then
@@ -16,8 +35,29 @@ function main() {
   else
     ${shebang_exe} "${@}"
   fi
-
-  exit 0
 }
+
+function sb_link() {
+  local filepath="$(cd "$(dirname "${1:-}")"; pwd)/$(basename "${1:-}")"
+  local name="${2:-}"
+
+  if [[ -z "${name}" ]]; then
+    echo name is empty
+    exit 1
+  fi
+
+  local linkpath="/usr/local/bin/${name}"
+  if [[ -e "${linkpath}" ]]; then
+    echo "${linkpath} is already exists"
+    exit 1
+  fi
+
+  cat << EOF | sudo tee "${linkpath}" >/dev/null
+#!/usr/bin/env bash
+sb "${filepath}" "\${@}"
+EOF
+  sudo chmod 755 "${linkpath}"
+}
+
 
 main "${@}"

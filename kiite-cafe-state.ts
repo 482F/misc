d6ac09cbe861170ba4f0d4443f2e0fcce8e1fcc0
482F <482F.git.mail@482f.net>
2024-06-21 00:00:00 +0900

    kiite-cafe の人数を state-viewer に送るスクリプトを作成

---
+++ kiite-cafe-state.ts
@@ -0,0 +1,37 @@
+#!/usr/bin/env -S deno run -A
+
+async function process() {
+  const nop = await fetch(
+    'https://cafeapi.kiite.jp/api/cafe/users?limit=50',
+    {},
+  )
+    .then((r) => r.json())
+    .then((j) => j.length) - 1
+
+  const border = 13
+
+  const { icon, backgroundColor } = (() => {
+    if (nop < border) {
+      return { icon: 'mdi-music-note-off-outline', backgroundColor: '#dddddd' }
+    } else {
+      return { icon: 'mdi-music-note-plus', backgroundColor: '#ffffbb' }
+    }
+  })()
+
+  const params = {
+    name: 'Kiite Cafe',
+    body: `${String(nop).padStart(2, ' ')}人`,
+    icon,
+    backgroundColor,
+  }
+
+  new Deno.Command(
+    '/mnt/e/data/git/state-viewer/dist/win-unpacked/state-viewer.exe',
+    {
+      args: Object.entries(params).map(([key, value]) => `${key}=${value}`),
+    },
+  ).spawn()
+}
+
+setInterval(process, 1000 * 60 * 10)
+process()
